e289dfd0033991d7ff42bfa1e60ce3f47a901e27
MelanX <meissi97@me.com>
2018-10-05 10:52:02 +0200

    compressed code

--- src/main/java/de/melanx/presentstand/block/presentStand/color/LimePresentStand.java
+++ src/main/java/de/melanx/presentstand/block/presentStand/color/LimePresentStand.java
@@ -1,102 +1,16 @@
 package de.melanx.presentstand.block.presentStand.color;
 
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
+import de.melanx.presentstand.block.presentStand.PresentStandBase;
 import net.minecraftforge.oredict.OreDictionary;
-import de.melanx.presentstand.PresentStand;
-import de.melanx.presentstand.block.BlockTileEntity;
-import de.melanx.presentstand.block.presentStand.TileEntityPresentStand;
 
-import javax.annotation.Nullable;
-
-public class LimePresentStand extends BlockTileEntity<TileEntityPresentStand> {
+public class LimePresentStand extends PresentStandBase {
 
 	public LimePresentStand() {
-		super(Material.SAND, "lime_present_stand");
-		setCreativeTab(CreativeTabs.DECORATIONS);
-		this.setHarvestLevel("pickaxe", 0);
-		setHardness(0.7f);
+		super("lime_present_stand");
 	}
-	
+
 	public void initOreDict() {
 		OreDictionary.registerOre("blockPresentStand", this);
-	}
-	
-	private static final AxisAlignedBB STAND_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.5625D, 0.9375D);
-	
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return STAND_AABB;
-	}
-
-	@Override
-	@Deprecated
-	public boolean isOpaqueCube(IBlockState state) {
-		return false;
-	}
-
-	@Override
-	@Deprecated
-	public boolean isFullCube(IBlockState state) {
-		return false;
-	}
-
-	@Override
-	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-		if (!world.isRemote) {
-			ItemStack heldItem = player.getHeldItem(hand);
-			TileEntityPresentStand tile = getTileEntity(world, pos);
-			IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side);
-			if (!player.isSneaking()) {
-				if (heldItem.isEmpty()) {
-					player.setHeldItem(hand, itemHandler.extractItem(0, 64, false));
-				} else {
-					player.setHeldItem(hand, itemHandler.insertItem(0, heldItem, false));
-				}
-				tile.markDirty();
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public void breakBlock(World world, BlockPos pos, IBlockState state) {
-		TileEntityPresentStand tile = getTileEntity(world, pos);
-		IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
-		ItemStack stack = itemHandler.getStackInSlot(0);
-		if (!stack.isEmpty()) {
-			EntityItem item = new EntityItem(world, pos.getX(), pos.getY(), pos.getZ(), stack);
-			world.spawnEntity(item);
-		}
-		super.breakBlock(world, pos, state);
-	}
-
-	@Override
-	public Class<TileEntityPresentStand> getTileEntityClass() {
-		return TileEntityPresentStand.class;
-	}
-
-	@Override
-	public boolean hasTileEntity(IBlockState state) {
-		return true;
-	}
-
-	@Nullable
-	@Override
-	public TileEntityPresentStand createTileEntity(World world, IBlockState state) {
-		return new TileEntityPresentStand();
 	}
 
 }
